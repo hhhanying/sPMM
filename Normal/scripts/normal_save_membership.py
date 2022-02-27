ed2f7c862e18bf6629594b1cb8cea9fa1bb93528
hhhanying <48273181+hhhanying@users.noreply.github.com>
2022-02-26 20:46:55 -0600

    add a configuration file for the true setting

---
+++ Normal/scripts/normal_save_membership.py
@@ -0,0 +1,15 @@
+'''
+This file will:
+
+Generate training set and test set.
+
+1. Train with supervised model;
+2. Use learned model to estimate membership for test set;
+3. Use learned model to estimate membership for test set under unsupervised model;
+4. Train with unsupervised model;
+5. Use learned model to estimate membership for test set.
+
+We will save:
+1. The True membership
+
+'''
