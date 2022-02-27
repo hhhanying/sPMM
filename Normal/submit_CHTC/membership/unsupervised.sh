067ee2cfd926fdb814b8c974e3a4f44bebcefb54
hhhanying <48273181+hhhanying@users.noreply.github.com>
2022-02-27 17:24:31 -0600

    change shell scripts to not only run test jobs

--- Normal/submit_CHTC/membership/unsupervised.sh
+++ Normal/submit_CHTC/membership/unsupervised.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export HOME=$_CONDOR_SCRATCH_DIR
-python ./unsupervised_membership.py "296" "configurations_membership.csv" "unsupervised_{}.txt" "dataset/dataset_{}.txt"
+python ./unsupervised_membership.py "$id" "configurations_membership.csv" "unsupervised_{}.txt" "dataset/dataset_{}.txt"
 
